a1b47dba3cfe9afc1c5a2e48766c72080be85576
anusha_v <manjuanusha037@gmail.com>
2026-09-24 20:44:22 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+// ===============================
+// MOBILE MENU
+// ===============================
+
+function toggleMenu() {
+
+    const navLinks = document.querySelector(".nav-links");
+
+    navLinks.classList.toggle("active");
+
+}
+
+
+// Close mobile menu after clicking a link
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        document
+            .querySelector(".nav-links")
+            .classList.remove("active");
+
+    });
+
+});
+
+
+// ===============================
+// CONTACT FORM
+// ===============================
+
+function sendMessage(event) {
+
+    event.preventDefault();
+
+    const name =
+        document.getElementById("name").value;
+
+    const email =
+        document.getElementById("email").value;
+
+    const subject =
+        document.getElementById("subject").value;
+
+    const message =
+        document.getElementById("message").value;
+
+
+    const mailSubject =
+        encodeURIComponent(subject);
+
+    const mailBody =
+        encodeURIComponent(
+            `Name: ${name}\n\nEmail: ${email}\n\nMessage:\n${message}`
+        );
+
+
+    const emailAddress =
+    "anushaanusha46572@gmail.com";
+
+
+    window.location.href =
+        `mailto:${emailAddress}?subject=${mailSubject}&body=${mailBody}`;
+
+}
